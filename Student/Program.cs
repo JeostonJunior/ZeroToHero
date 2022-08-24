8446d6e6d4cb1e18e2a45ae685a7c4eff1b42968
Jeoston Araújo <jeostonjunior@gmail.com>
2022-08-24 15:15:17 -0300

    new Class Student

---
+++ Student/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            student.Name = "Alex Green";
+            student.SetNotes();
+            student.FinalNotes();
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Globalization;
+namespace Student
+{
+    public class Student
+    {
+        public string Name;
+        public double TestNote1, TestNote2, TestNote3;
+
+        public void SetNotes()
+        {
+            Console.WriteLine($"Student Name: {Name}");
+            Console.WriteLine($"Insert Student note One:");
+            TestNote1 = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"Insert Student note Two:");
+            TestNote2 = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"Insert Student note Three:");
+            TestNote3 = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        public void FinalNotes()
+        {
+            double finalNotes;
+            finalNotes = TestNote1 + TestNote2 + TestNote3;
+            Console.WriteLine($"The Final Note: {finalNotes}");
+
+            if (finalNotes >= 60.00)
+            {
+                Console.WriteLine($"APPROVED");
+            }
+            else
+            {
+                Console.WriteLine($"DISAPPROVED");
+                Console.WriteLine($"You need {60 - finalNotes} poits");
+            }
+        }
+    }
+}
