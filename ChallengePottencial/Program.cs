e75487725c2a9a7d6429ca99e10674e574315915
Jeoston Araújo <jeostonjunior@gmail.com>
2022-08-29 23:36:31 -0300

    Challenge Pottencial complete

---
+++ ChallengePottencial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class ChallengePottencial
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        int iterator = 0;
+        int sumNumbers = 0;
+        int productNumbers = 1;
+
+        while (number > 0 && number <= Math.Pow(10, 5))
+        {
+            iterator = number % 10;
+            sumNumbers += iterator;
+            productNumbers *= iterator;
+            number /= 10;
+        }
+        Console.WriteLine($"Result: {productNumbers - sumNumbers}");
+    }
+}
