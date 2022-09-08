ee14d07d97be22523980bf88ca1fbf9fe6aa9790
Jeoston Araújo <jeostonjunior@gmail.com>
2022-09-07 23:38:12 -0300

    Vetores

---
+++ Vetores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+namespace Vetores
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetor;
+            int n = int.Parse(Console.ReadLine());
+
+            vetor = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vetor[i];
+            }
+            Console.WriteLine($"A soma de todos os numeros é {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            double avg = sum / n;
+
+            Console.WriteLine($"A media dos valores é {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
