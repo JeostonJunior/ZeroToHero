5cb859918489482c38f89f727e9c841fbeb779c5
Jeoston Araújo <jeostonjunior@gmail.com>
2022-09-10 21:35:26 -0300

    usando params, para iniciar infinitos parametros de entrada

---
+++ Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Sum(10,20);
+            Console.WriteLine($"{result}");
+        }
+    }
+}
