9bf5606acf545aa2ff9db0f71835fa71a08de966
Jeoston Araújo <jeostonjunior@gmail.com>
2022-09-15 21:58:33 -0300

    Matriz de ordem N com diagonal principal e numeros negativos

---
+++ Matrizes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz;
+            Console.WriteLine($"Qual a dimensão da matriz?");
+            int ordemMatriz = int.Parse(Console.ReadLine());
+            matriz = new int[ordemMatriz,ordemMatriz];
+
+            for (int i = 0; i < ordemMatriz; i++)
+            {
+                Console.WriteLine($"Informe os valores da linha {i+1}:");
+                string[] valMatriz = Console.ReadLine().Split(' ');
+                for (int x = 0; x < ordemMatriz; x++)
+                {
+                    matriz[i,x] = int.Parse(valMatriz[x]);
+                }
+            }            
+
+            Console.WriteLine($"Main Diagonal:");
+            for (int k = 0; k < matriz.GetLength(1); k++)
+            {
+                Console.Write($"|{matriz[k,k]}| ");
+            }        
+
+
+            int negativeNumbers = 0;
+            foreach (var number in matriz)
+            {
+                if(number < 0){
+                    negativeNumbers++;
+                }
+            }
+            Console.WriteLine($"Negative Numbers: {negativeNumbers}");            
+        }
+    }
+}
