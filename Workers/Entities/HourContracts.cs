2ee28f7905c4f6426505fdb7284ba364431fef12
Jeoston Araújo <jeostonjunior@gmail.com>
2022-09-22 23:51:55 -0300

    Projeto Finalizado, com uso de OO, laços e Emum

---
+++ Workers/Entities/HourContracts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Workers.Entities
+{
+    public class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hour { get; set; }
+        public HourContract () { }
+        public HourContract (DateTime date, double valuePerHour, int hour)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hour = hour;
+        }
+        public double totalValue ()
+        {
+            return ValuePerHour * Hour;
+        }
+    }
+}
---
+++ Workers/Entities/Worker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Workers.Entities.Enums;
+
+namespace Workers.Entities
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> HourContracts { get; set; } = new List<HourContract>();
+
+        public Worker ()
+        {
+        }
+        public Worker (string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+        public void AddContract (HourContract contract)
+        {
+            HourContracts.Add(contract);
+        }
+        public void RemoveContract (HourContract contract)
+        {
+            HourContracts.Remove(contract);
+        }
+        public double Income (int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in HourContracts)
+            {
+                if (contract.Date.Month.Equals(month) && contract.Date.Year.Equals(year))
+                {
+
+                    sum += contract.totalValue();
+                }
+            }
+            return sum;
+
+        }
+        public override string ToString ()
+        {
+            return $"Name: {Name} | Level: {Level} | BaseSalary: {BaseSalary}";
+        }
+    }
+}
