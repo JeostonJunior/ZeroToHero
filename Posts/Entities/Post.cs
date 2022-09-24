e07a5d5e464d11362bf427d11b0ec3f95932578f
Jeoston Araújo <jeostonjunior@gmail.com>
2022-09-24 16:34:45 -0300

    Clas Post, usando o System.Text com o metodo StringBuilder

---
+++ Posts/Entities/Post.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posts.Entities
+{
+    internal class Post
+    {
+        public DateTime Momment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        public List<Comment> Comments = new List<Comment> ();
+
+        public Post () { }
+        public Post (DateTime momment, string title, string content, int likes)
+        {
+            Momment = momment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+
+        public void PublicPost ()
+        {
+            Console.WriteLine($"{Momment}\n{Title}\n{Content}\n{Likes}\nComments:");
+            foreach (Comment comment in Comments)
+            {
+                Console.WriteLine(comment.Text);
+            }
+        }
+
+        public override string ToString ()
+        {
+            StringBuilder stringBuilder = new StringBuilder ();
+            stringBuilder.AppendLine(Momment.ToString("dd/MM/yyyy HH:mm:ss"));
+            stringBuilder.AppendLine(Title);
+            stringBuilder.AppendLine(Content);
+            stringBuilder.Append(Likes);
+            stringBuilder.AppendLine(" - Likes");
+            stringBuilder.AppendLine("Commments:");
+
+            foreach (Comment comment in Comments)
+            {
+                stringBuilder.AppendLine(comment.Text);
+            }
+            return stringBuilder.ToString();
+
+        }
+
+    }
+}
---
+++ Posts/Program.cs
@@ -0,0 +1,31 @@
+﻿using Posts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posts
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            DateTime momment = new DateTime(2018, 06, 21, 13, 05, 44);
+            string title = "Traveling to New Zealand";
+            string content = "I'm going to visit this wondergul country!";
+            int likes = 12;
+            Comment comment1 = new Comment("Have a nice trip");
+            Comment comment2 = new Comment("Wow that's awesome!");
+
+            Post post1 = new Post(momment, title, content, likes);
+
+            post1.AddComment(comment1);
+            post1.AddComment(comment2);
+
+            Console.WriteLine(post1);
+
+
+        }
+    }
+}
