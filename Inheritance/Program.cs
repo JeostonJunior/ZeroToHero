8b69755a636eba66e73005a0488169a61b73a3e4
Jeoston Araújo <jeostonjunior@gmail.com>
2022-09-24 23:42:46 -0300

    Usando Herança e Protected

---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using Inheritance.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(5000.0, 0001, "Big", 55000.00);
+
+            Console.WriteLine(account);
+        }
+    }
+}
