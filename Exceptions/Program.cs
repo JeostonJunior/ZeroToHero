e48f65d0860d7e5cece48dac9bf8b297b2cad711
Jeoston Araújo <jeostonjunior@gmail.com>
2022-10-03 23:42:25 -0300

    Exception Handling

---
+++ Exceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            int n1, n2, result;
+            try
+            {
+                n1 = int.Parse(Console.ReadLine());
+                n2 = int.Parse(Console.ReadLine());
+                result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- HttpServer/Server.js
+++ HttpServer/Server.js
@@ -1,30 +1,5 @@
-//Para usar o servidor http deve-se dar um require no modulo http do nodeJs
 const http = require("http");
 
-//REQ = Request --- RES = Response
-//req.url pega o cabeçalho do url
-//res.end responde ao req diretamente no navegador
-//A função createServer é uma função que recebe 2 parametros de entrada, uma request e uma response
-//A request é responsavel por pegar as informações passadas no cabeçalho da página(url), e a response é as informações
-//que serão respondidas quando for solicitada uma request
-
-/*
-A request nescessita de um status que é passado no cabeçalho da página
--- Ex http://localhost:3000/Contatos
-Ela também depende de um host nessa caso um IP e PORTA
--- Ex localhost:3000
-
-> new URL(request.url, `http://${request.getHeaders().host}`)
-URL {
-  href: 'http://localhost:3000/contato',
-  origin: 'http://localhost:3000',
-  protocol: 'http:',
-  host: 'localhost:3000',
-  port: '3000',
-}
-*/
-
-// Esse processo é feito por um GET, ou seja as informações são requisitadas e não passadas
 const server = http.createServer((req, res) => {
     switch (req.url.toUpperCase()) {
         case "/":
@@ -42,17 +17,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-/*
---res.end é um função que quando ocorre uma request com o status solicitado, o res.end responde atraves
-de um response diretamente dentro da pagina html do server http
-*/
-
-//Listen precisa de uma porta e um endereço para funcionar
-//O servidor http está rodando na porta 3001 e no endereço localhost
-/*
-O server listen é uma função assincrona que recebe a PORTA e IP em que o servidor será iniciado,
-portanto fica 'observando' a porta e o ip até que o servidor seja encerrado ou ocorra um erro que quebre o código
-*/
 server.listen(3000, "localhost", () => {
     console.log("Hospedado em http://localhost:3000");
     console.log("ctrl + c para sair");
