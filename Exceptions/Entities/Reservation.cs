cdeef43762a2f160ca7bf35ad31dd384feb0f3a9
Jeoston Araújo <jeostonjunior@gmail.com>
2022-10-08 22:11:01 -0300

    Atualização da classe resrvation, delegando a logica de reserva para o metodo de atualização, classe main ainda está com delegação errada

--- Exceptions/Entities/Reservation.cs
+++ Exceptions/Entities/Reservation.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exceptions.Entities
 {
@@ -12,25 +8,38 @@ namespace Exceptions.Entities
         public DateTime CheckIn { get; protected set; }
         public DateTime CheckOut { get; protected set; }
 
-        public Reservation (int roomNumber, DateTime checkIn, DateTime checkOut)
+        public Reservation (int roomNumber)
         {
             RoomNumber = roomNumber;
+        }
+        public Reservation (int roomNumber, DateTime checkIn, DateTime checkOut) : this (roomNumber)
+        {
             CheckIn = checkIn;
             CheckOut = checkOut;
         }
-
         public int Duration ()
         {
             TimeSpan duration = CheckOut.Subtract(CheckIn);
             return (int)duration.TotalDays;
         }
-
-        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        public string UpdateDates (DateTime checkIn, DateTime checkOut)
         {
-            CheckIn = checkIn;
-            CheckOut = checkOut;
+            DateTime now = DateTime.Now;
+            if (checkOut < now || checkIn < now)
+            {
+                return "Erro na Reserva: As datas de reserva devem ser futuras;";
+            }
+            else if (checkOut <= checkIn)
+            {
+                return "Erro na Reserva: A data de Check-out deve ser maior que Check-in;";
+            }
+            else
+            {
+                CheckIn = checkIn;
+                CheckOut = checkOut;
+                return "Reserva Atualizada";
+            }
         }
-
         public override string ToString ()
         {
             return $"Reservation: Room {RoomNumber}, Check-in: {CheckIn.ToString("dd/MM/yyyy")}, Check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights";
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -9,33 +9,29 @@ namespace Exceptions
         {
 			try
 			{
-				Console.WriteLine("Entre com os dados da reserva:");
+				
+
+                Console.WriteLine("Entre com os dados da reserva:");
 				Console.Write("Numero do Quarto: ");
 				int roomNumber = int.Parse(Console.ReadLine());
 				Console.Write("CheckIn: ");
 				DateTime checkIn = DateTime.Parse(Console.ReadLine());
                 Console.Write("CheckOut: ");
                 DateTime checkOut = DateTime.Parse(Console.ReadLine());
+                Reservation reservation = new Reservation(roomNumber);
 
-                DateTime now = DateTime.Now;
-                while (checkIn < now || checkOut < now)
+                string erroReservation = reservation.UpdateDates(checkIn, checkOut);
+
+                while(erroReservation != "Reserva Atualizada")
                 {
-                    Console.WriteLine("As reservas devem ser para datas futuras;");
+                    Console.WriteLine(erroReservation);
                     Console.Write("CheckIn: ");
                     checkIn = DateTime.Parse(Console.ReadLine());
                     Console.Write("CheckOut: ");
                     checkOut = DateTime.Parse(Console.ReadLine());
+                    erroReservation = reservation.UpdateDates(checkIn, checkOut);
                 }
-                while (checkOut <= checkIn)
-				{
-					Console.WriteLine("Data de Reserva Invalida, entre novamente com os dados;");
-                    Console.Write("CheckIn: ");
-                    checkIn = DateTime.Parse(Console.ReadLine());
-                    Console.Write("CheckOut: ");
-                    checkOut = DateTime.Parse(Console.ReadLine());
-                }
-				Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
-				Console.WriteLine(reservation);
+                Console.WriteLine(reservation);
             }
 			catch (Exception e)
 			{
