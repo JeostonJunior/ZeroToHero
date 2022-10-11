21a3cf993626c051383510bee3e8b33c36b287ca
Jeoston Araújo <jeostonjunior@gmail.com>
2022-10-10 21:57:52 -0300

    Uso do FileInfo e File, para leitura e copia de arquivos, erros são tratados no IOException

---
+++ TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoComArquivos
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            string sourcePath = @"c:\Users\jeoston.araujo\Documents\ZeroToHero\TrabalhandoComArquivos\text1.txt";
+            string targetPath = @"c:\Users\jeoston.araujo\Documents\ZeroToHero\TrabalhandoComArquivos\text3.txt";
+
+            try
+            {
+                //FileInfo fileInfo = new FileInfo(sourcePath);
+                //fileInfo.CopyTo(targetPath);
+                //File.WriteAllLines(targetPath, File.ReadAllLines(sourcePath));
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    File.WriteAllText(targetPath, line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+
+            }
+
+
+        }
+    }
+}
