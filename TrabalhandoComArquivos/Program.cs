922321526b3b5d9c2e466e18992f892fa7d49a10
Jeoston Araújo <jeostonjunior@gmail.com>
2022-10-11 22:12:45 -0300

    bloco using e cascateamento

--- TrabalhandoComArquivos/Program.cs
+++ TrabalhandoComArquivos/Program.cs
@@ -9,40 +9,60 @@ namespace TrabalhandoComArquivos
         {
             string sourcePath = @"c:\Users\jeoston.araujo\Documents\ZeroToHero\TrabalhandoComArquivos\text1.txt";
             //string targetPath = @"c:\Users\jeoston.araujo\Documents\ZeroToHero\TrabalhandoComArquivos\text3.txt";
-            FileStream fs = null;
-            StreamReader sr = null;
-
-
+            //FileStream fs = null;
+            //StreamReader sr = null;
             try
             {
-                ////FileInfo fileInfo = new FileInfo(sourcePath);
-                ////fileInfo.CopyTo(targetPath);
-                ////File.WriteAllLines(targetPath, File.ReadAllLines(sourcePath));
-                //string[] lines = File.ReadAllLines(sourcePath);
-                //foreach (string line in lines)
-                //{
-                //    File.WriteAllText(targetPath, line);
-                //}
-                //fs = new FileStream(sourcePath, FileMode.Open);
-                //sr = new StreamReader(fs);
-
-                sr = File.OpenText(sourcePath);
-                string line = sr.ReadToEnd();
-                Console.WriteLine(line);
-                
+                using (FileStream fs = new FileStream(sourcePath, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        Console.WriteLine(sr.ReadToEnd());
+                    }
+                }
             }
             catch (IOException e)
             {
-                Console.WriteLine($"An error occurred: {e.Message}");
+                Console.WriteLine(e.Message);
+            }
 
-            }
-            finally
-            {
-                if(sr != null)
-                {
-                    sr.Close();
-                }         
-            }
+
+
+            //try
+            //{
+            //    ////FileInfo fileInfo = new FileInfo(sourcePath);
+            //    ////fileInfo.CopyTo(targetPath);
+            //    ////File.WriteAllLines(targetPath, File.ReadAllLines(sourcePath));
+            //    //string[] lines = File.ReadAllLines(sourcePath);
+            //    //foreach (string line in lines)
+            //    //{
+            //    //    File.WriteAllText(targetPath, line);
+            //    //}
+            //    //fs = new FileStream(sourcePath, FileMode.Open);
+            //    //sr = new StreamReader(fs);
+
+            //    //sr = File.OpenText(sourcePath);
+            //    //string line = sr.ReadToEnd();
+            //    //Console.WriteLine(line);
+
+
+
+
+
+
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine($"An error occurred: {e.Message}");
+
+            //}
+            //finally
+            //{
+            //    if(sr != null)
+            //    {
+            //        sr.Close();
+            //    }         
+            //}
         }
     }
 }
