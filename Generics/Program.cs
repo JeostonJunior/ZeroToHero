2efe72442beb0a6e4cd94c80c5d0f4a9c410bcf6
Jeoston Araújo <jeostonjunior@gmail.com>
2022-11-01 00:53:00 -0300

    mods

---
+++ Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using Generics.Services;
+using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("How many values? ");
+            int value = int.Parse(Console.ReadLine());
+            PrintService<string> printService = new PrintService<string>();       
+            for (int i = 0; i < value; i++)
+            {
+                string number = Console.ReadLine();
+                printService.AddValue(number);
+            }
+            printService.Print();
+            Console.WriteLine($"First: {printService.First()}");
+        }
+    }
+}
---
+++ Generics/Services/PrintService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics.Services
+{
+    public class PrintService<T>
+    {
+        public List<T> Numbers { get; set; } = new List<T>();
+        public void AddValue (T value)
+        {
+            Numbers.Add(value);
+        }
+        public T First ()
+        {
+            return Numbers.First();
+        }
+        public void Print ()
+        {
+            Console.Write("[");
+            Console.Write(string.Join(",", Numbers));
+            Console.Write("]\n");
+        }
+    }
+}
